f3ee350cef959c9c0c5e17e287d32e12ce9b1fc9
Kritika Singh <kritika.singh1994@gmail.com>
2019-01-28 15:48:32 +0530

    Pratibha, Kritika | 11 | Added footer.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
-import { Route } from 'react-router';
-import { ConnectedRouter } from 'react-router-redux';
-import { Provider } from 'react-redux';
+import {Route} from 'react-router';
+import {ConnectedRouter} from 'react-router-redux';
+import {Provider} from 'react-redux';
 import configureStore from './store';
 import createHistory from 'history/createBrowserHistory'
 import Header from './Header'
 import Home from './Home'
+import Footer from "./Footer";
 
 const browserHistory = createHistory()
 
 const store = configureStore(browserHistory);
 
 const Routes = () => (
-  <ConnectedRouter history={browserHistory}>
-    <Route component={Home} path="/" />
-  </ConnectedRouter>
+    <ConnectedRouter history={browserHistory}>
+        <Route component={Home} path="/"/>
+    </ConnectedRouter>
 );
 
 const Main = () => (
-  <div>
-    <Header />
-    <Routes />
-  </div>  
+    <div>
+        <Header/>
+        <Routes/>
+        <Footer/>
+    </div>
 );
 
 const App = () => (
-  <Provider store={store} >
-    <Main />
-  </Provider>
+    <Provider store={store}>
+        <Main/>
+    </Provider>
 );
 
 export default App;
